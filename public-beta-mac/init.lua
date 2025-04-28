ac6e75f707125f9df2b26ec114076fc4255e7b82
asdwert7 <asdwert309@gmail.com>
2025-04-28 21:03:27 +0300

    Update#3
    Перенастроена скорость для конфига init.lua

--- public-beta-mac/init.lua
+++ public-beta-mac/init.lua
@@ -1,47 +1,61 @@
-local eventtap   = hs.eventtap
-local keycodes   = hs.keycodes
-local utf8       = utf8
+local eventtap = hs.eventtap
+local keycodes = hs.keycodes
+local utf8 = utf8
+local math = math
 
--- Параметры (можешь подкорректировать)
-local blockSize          = 10000        -- печатать блоками по 5 символов
-local layoutSwitchDelay  = 100000   -- 100 мс после переключения раскладки
-local betweenCharsDelay  = 1000    -- 1 мс между символами
-local betweenBlocksDelay = 200000   -- 200 мс между блоками
+-- ⚙️ НАСТРОЙКИ СКОРОСТИ (меняйте эти значения!)
+local settings = {
+    -- Основные задержки (в микросекундах! 1000 мкс = 1 мс)
+    minDelay = 30000,     -- 30 мс (минимальная пауза между символами)
+    maxDelay = 250000,    -- 250 мс (максимальная пауза)
+    
+    -- Дополнительные параметры
+    lineEndDelay = 300000, -- 300 мс после Enter
+    layoutSwitchDelay = 100000, -- 100 мс на переключение раскладки
+    mistakeDelay = 150000, -- 150 мс при "ошибке"
+}
 
--- Проверка, русский ли символ
+-- Проверка, русский ли символ (без изменений)
 local function isRussianChar(char)
     local cp = utf8.codepoint(char)
     return (cp >= 0x0410 and cp <= 0x044F) or cp == 0x0401 or cp == 0x0451
 end
 
+-- Функция ввода символа с задержкой
+local function humanTypeChar(char)
+    eventtap.keyStrokes(char)
+    hs.timer.usleep(math.random(settings.minDelay, settings.maxDelay))
+end
+
+-- Основная функция
 hs.hotkey.bind({"ctrl", "shift"}, "v", function()
     local text = hs.pasteboard.getContents()
     if not text or #text == 0 then return end
-
+    
     local originalLayout = keycodes.currentSourceID()
-
-    for start = 1, #text, blockSize do
-        local block = text:sub(start, start + blockSize - 1)
-        for _, cp in utf8.codes(block) do
+    local isRussian = false
+    
+    for line in text:gmatch("[^\r\n]+") do
+        for _, cp in utf8.codes(line) do
             local char = utf8.char(cp)
-
-            -- 1) переключаем раскладку
-            if isRussianChar(char) then
-                keycodes.currentSourceID("com.apple.keylayout.RussianWin")
-            else
-                keycodes.currentSourceID("com.apple.keylayout.US")
+            local needRussian = isRussianChar(char)
+            
+            -- Переключение раскладки (с задержкой)
+            if needRussian ~= isRussian then
+                keycodes.currentSourceID(needRussian and "com.apple.keylayout.RussianWin" or "com.apple.keylayout.US")
+                hs.timer.usleep(settings.layoutSwitchDelay)
+                isRussian = needRussian
             end
-            hs.timer.usleep(layoutSwitchDelay)
-
-            -- 2) печатаем символ через keyStrokes
-            eventtap.keyStrokes(char)
-            hs.timer.usleep(betweenCharsDelay)
+            
+            humanTypeChar(char) -- Печать символа
         end
-
-        -- пауза после каждого блока
-        hs.timer.usleep(betweenBlocksDelay)
+        
+        -- Перенос строки (кроме последней)
+        if line ~= "" then
+            eventtap.keyStroke({}, "return")
+            hs.timer.usleep(settings.lineEndDelay)
+        end
     end
-
-    -- возвращаем прежнюю раскладку
-    keycodes.currentSourceID(originalLayout)
+    
+    keycodes.currentSourceID(originalLayout) -- Возврат раскладки
 end)
